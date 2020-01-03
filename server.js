2da83ea6c39f79d85a7354c664740246ef14ddca
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2020-01-03 09:18:21 -0500

    added express, get, and listen

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 var express = require('express');
 
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Testing the route');
+})
+
+app.listen(3000, function(){
+    console.log('Running on port 3000');
+})
